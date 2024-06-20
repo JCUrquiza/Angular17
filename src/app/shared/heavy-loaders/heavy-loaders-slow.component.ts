3accc38c24f3e52fdd87be907c9dcc38ef48ab86
Juan Carlos <gurquiza8@gmail.com>
2024-06-20 16:17:03 -0600

    Working with interactions, next level of lazy components

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
-  template: `<h1>Hey world!</h1>`
+  imports: [ CommonModule ],
+  template: `
+    <section [ngClass]="['w-full h-[600px]', cssClass]">
+      Heavy Loader Component
+    </section>
+  `
 })
 export class HeavyLoadersSlowComponent {
+
+  @Input({ required: true }) cssClass!: string;
+
+  constructor() {
+
+    const start = Date.now();
+    while( Date.now() - start < 1200 ) {}
+
+    console.log('Cargando');
+  }
 
 }
