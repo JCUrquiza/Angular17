979ffd766cca97396579b16a693f536d8b677151
Juan Carlos <gurquiza8@gmail.com>
2024-06-20 13:18:33 -0600

    Implementing new syntax of: switch using signals

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 
+
+type Grade = 'A' | 'B' | 'F';
+
 @Component({
   standalone: true,
   imports: [],
@@ -9,6 +12,7 @@ import { Component, signal } from '@angular/core';
 export default class ControlFlowComponent {
 
   public showContent = signal(false);
+  public grade = signal<Grade>('A');
 
   public toggleContent() {
     this.showContent.update( value => !value );
