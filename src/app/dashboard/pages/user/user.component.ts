04f0f495207de8e40d641fb403b912e67eee6a07
Juan Carlos <gurquiza8@gmail.com>
2024-06-22 12:51:06 -0600

    Parsed observable to signal (with toSignal method)

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+import { TitleComponent } from '@shared/title/title.component';
+import { User } from '../../../interfaces/req-response';
+import { UserService } from '@services/user.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styles: ``
+  imports: [ TitleComponent ],
+  template: `
+    <app-title title="User" />
+
+    @if ( user() ) {
+      <section>
+        <img
+          [srcset]="user()!.avatar"
+          [alt]="user()!.first_name"
+        />
+
+        <div>
+          <h3>{{ user()!.first_name }} {{ user()!.last_name }}</h3>
+          <p>{{ user()!.email }}</p>
+        </div>
+
+      </section>
+    } @else {
+      <p>Cargando información</p>
+    }
+
+  `,
 })
 export default class UserComponent {
+
+  private route = inject( ActivatedRoute );
+  private userService = inject( UserService );
+
+  // public user = signal<User | undefined>(undefined);
+  // toSignal: nos permite tomar un observable y regresar una señal:
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap( ({ id }) => this.userService.getUserById( id ) )
+    )
+  )
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User, UsersResponse } from '../interfaces/req-response';
-import { delay } from 'rxjs';
+import type { User, UserResponse, UsersResponse } from '../interfaces/req-response';
+import { delay, map } from 'rxjs';
 
 interface State {
   users: User[];
@@ -28,13 +28,22 @@ export class UserService {
     this.http.get<UsersResponse>('https://reqres.in/api/users')
       .pipe( delay(1500) )
       .subscribe( res => {
-
         this.#state.set({
           loading: false,
           users: res.data
         });
-
       });
+
+  }
+
+
+  getUserById( id: string ) {
+
+    return this.http.get<UserResponse>(`https://reqres.in/api/users/${ id }`)
+      .pipe(
+        delay(1500),
+        map( resp => resp.data )
+      )
 
   }
 
