74b5ac803d0cd6a61505acb18b0e259cdb7b88ab
Juan Carlos <gurquiza8@gmail.com>
2024-06-22 21:49:24 -0600

    Implementing components of Angular Material 3

--- src/app/dashboard/pages/material/material.component.ts
+++ src/app/dashboard/pages/material/material.component.ts
@@ -1,15 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import {
+  MatBottomSheet,
+  MatBottomSheetModule,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
+
+import { OptionsButtomSheetComponent } from './ui/options-buttom-sheet/options-buttom-sheet.component';
 
 @Component({
   selector: 'app-material',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+  ],
   templateUrl: './material.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class MaterialComponent {
 
+  constructor(
+    private _bottomSheet: MatBottomSheet
+  ) {}
 
+  openButtomSheet() {
+    this._bottomSheet.open(OptionsButtomSheetComponent);
+  }
 
 }
---
+++ src/app/dashboard/pages/material/ui/options-buttom-sheet/options-buttom-sheet.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-options-buttom-sheet',
+  standalone: true,
+  imports: [
+    MatListModule
+  ],
+  templateUrl: './options-buttom-sheet.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class OptionsButtomSheetComponent {
+
+  openLink(event: MouseEvent) {
+    console.log('openLink', event);
+  }
+
+}
